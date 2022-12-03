e0bd62ff17f172e0b7231281bad92121755aa659
Bhavya Bansal <bhavyabansal0916@gmail.com>
2022-12-03 23:10:46 +0530

    GuessCommit

---
+++ Assignment 3/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Assignment_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Okbtn = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.Canbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(207, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(367, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GUESS THE NUMBER";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(309, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(226, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(304, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "PLAY AGAIN OR NOT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Javanese Text", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(218, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(323, 43);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter A Number Between 1 to 50";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(197, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(368, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Do you want to play this game?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(199, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Enter A Number :\r\n";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(376, 259);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(190, 305);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 34);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Yes i Want !";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Okbtn
+            // 
+            this.Okbtn.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Okbtn.Location = new System.Drawing.Point(190, 367);
+            this.Okbtn.Name = "Okbtn";
+            this.Okbtn.Size = new System.Drawing.Size(114, 32);
+            this.Okbtn.TabIndex = 8;
+            this.Okbtn.Text = "OK";
+            this.Okbtn.UseVisualStyleBackColor = true;
+            this.Okbtn.Click += new System.EventHandler(this.Okbtn_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(452, 305);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(122, 34);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "No I\'m Done";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Canbtn
+            // 
+            this.Canbtn.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Canbtn.Location = new System.Drawing.Point(455, 367);
+            this.Canbtn.Name = "Canbtn";
+            this.Canbtn.Size = new System.Drawing.Size(119, 32);
+            this.Canbtn.TabIndex = 10;
+            this.Canbtn.Text = "CANCEL";
+            this.Canbtn.UseVisualStyleBackColor = true;
+            this.Canbtn.Click += new System.EventHandler(this.Canbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 411);
+            this.Controls.Add(this.Canbtn);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.Okbtn);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button Okbtn;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button Canbtn;
+    }
+}
+
---
+++ Assignment 3/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random random = new Random();
+        int randno;
+        int count = 1;
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label4.Visible = false;
+            textBox1.Visible = false;
+            Okbtn.Visible = false;
+            Canbtn.Visible = false;
+            button2.Visible = false;
+            label3.Visible = false;
+            button3.Visible = false;
+            button5.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            label2.Visible = true;
+            label4.Visible = true;
+            textBox1.Visible = true;
+            Okbtn.Visible = true;
+            Canbtn.Visible = true;
+            randno = random.Next(1, 50);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label3.Visible = true;
+            button3.Visible = true;
+            button5.Visible = true;
+        }
+
+        private void Canbtn_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void Okbtn_Click(object sender, EventArgs e)
+        {
+            int input = int.Parse(textBox1.Text);
+            if (input == 0)
+                return;
+            else if (input < randno)
+            {
+                String message = "Low Number ,Try Again!!";
+                MessageBox.Show(message, "Error Message", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                textBox1.Text = "";
+                count += 1;
+
+            }
+            else if (input > randno)
+            {
+                String message = "High Number ,Try Again!!";
+                MessageBox.Show(message, "Error Message", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                textBox1.Text = "";
+                count += 1;
+            }
+            else if (input == randno)
+            {
+                Console.WriteLine(count);
+                MessageBox.Show("You Gussed it! The Number Was " + randno.ToString(), "YOU WON!!");
+                MessageBox.Show("It took you " + count.ToString() + " tries.\n", "YOU WON!!");
+                button1.Visible = false;
+                button2.Visible = true;
+                label2.Visible = false;
+                label4.Visible = false;
+                textBox1.Visible = false;
+                Okbtn.Visible = false;
+                Canbtn.Visible = false;
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            label3.Visible = false;
+            button3.Visible = false;
+            button5.Visible = false;
+            button1.Visible = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+       
+    }
+}
